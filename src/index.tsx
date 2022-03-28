3b27f986827ec302c43973421d7d9b17f5f1c358
Big Shuang <3508645145>
2022-03-28 10:33:15 +0800

    basic done

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { TodoApp } from './TodoApp.tsx';
+import './style.css'
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('app')
+)
